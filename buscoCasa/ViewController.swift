017fee4874eced0168dcc2780aed7223540f7090
Rodrigo Pintos <rodrigo.pintos@mercadolibre.com>
2019-05-20 22:56:42 -0300

    Initial Commit

---
+++ buscoCasa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buscoCasa
+//
+//  Created by Rodrigo Pintos on 5/20/19.
+//  Copyright © 2019 Rodrigo Pintos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
